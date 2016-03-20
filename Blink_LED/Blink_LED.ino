8edbc6b959c94b8fead0e7097327c8be1ffa5a59
bmaulana <bmaulana273@gmail.com>
2016-03-20 22:36:19 +0000

    add modified file base

---
+++ Blink_LED/Blink_LED.ino
@@ -0,0 +1,14 @@
+#define LED RED_LED
+
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(LED, OUTPUT);     
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  digitalWrite(LED, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);               // wait for a second
+  digitalWrite(LED, LOW);    // turn the LED off by making the voltage LOW
+  delay(1900);               // wait for a second
+}
